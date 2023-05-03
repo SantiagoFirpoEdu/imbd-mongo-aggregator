42cce1dbc27e9db7ef02f6131e99b65ff20c5553
Santiago <s.firpo@edu.pucrs.br>
2023-05-03 00:19:28 -0300

    Created .NET project to convert tsvs to json

---
+++ imdb-to-json/imdb-to-json/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+Console.WriteLine("Hello, World!");
+
+CsvConfiguration tsvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    Delimiter = "\t",
+    HasHeaderRecord = true
+};
+
+using StreamReader fileReader = new StreamReader("");
+using CsvReader csvReader = new CsvReader(fileReader, tsvConfig);
+var a = csvReader.GetRecords<>()
